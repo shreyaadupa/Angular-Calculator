e77795b22ab3244ae878cf19cd78229ca3d76da6
shreyaadupa <81499294+shreyaadupa@users.noreply.github.com>
2022-09-08 15:21:07 -0400

    2 files modified

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -8,8 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class CalcComponent implements OnInit {
 
   constructor() { }
+  //every time a number is pressed add it to the array in the order it is pressed
+  //when calling the methods take the array param and execute the operation between each number present
+
+  public nums = [];
+  public answer;
 
   ngOnInit() {
   }
+
+  add(nums)
+  {
+    for(let i = 0; i<nums.length; i++)
+    {
+      this.answer+=nums[i];
+      nums[i]=0;
+    }
+    return this.answer;
+  }
+
+  
 
 }
